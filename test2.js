4164fd5c54ebc6335f05227ad866c9da48a1a16a
Haider Rahman <haiderr.rahman@gmail.com>
2026-09-07 07:37:19 +0300

    Remove test script

--- test2.js
+++
@@ -1,6 +0,0 @@
-async function test() {
-  const url = 'https://maps.app.goo.gl/1ZGUE8B1fUtisJjw5';
-  const response = await fetch(url, { redirect: 'follow' });
-  console.log(response.url);
-}
-test();
