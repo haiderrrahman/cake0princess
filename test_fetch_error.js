da990d203b693c6ea6f2514a715d3e2efcd9e184
Haider Rahman <haiderr.rahman@gmail.com>
2026-09-07 07:52:35 +0300

    Remove test files

--- test_fetch_error.js
+++
@@ -1,9 +0,0 @@
-async function test() {
-  try {
-    const url = "مجمع B عمارة 304 شقة 101 https://maps.app.goo.gl/xyz";
-    await fetch(url);
-  } catch (e) {
-    console.error("Error:", e.message);
-  }
-}
-test();
